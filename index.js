37f32f823baf0758027f8b9b73c8cc7f1a48063f
gattiagus <agustinegatti10@gmail.com>
2024-08-14 19:44:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,75 +1,76 @@
+const productos = {
+    Campera: 200,
+    Buzo: 300,
+    Remera: 600,
+    Pantalon: 400,
+    Bermuda: 100
+};
 
-/*
-Programa:
- 
- Desc: un sistema de cobro
- - Pago contado , 0 Interes
- - Pago en credito <= 3 , con el 5% de inteneres
- - Pago en credito > 3 < 8  , con el 10% de interes
- - Pago en credito > 8 , con el 15% de interes
- - Indicar cuanto sale la cuota
-*/
-/* Functions */
-function msgTotalPayCounted(num) {
-    console.log("Su pago al contado es de "+num+"$");
-}
-function msgTotalPayCredit(totalPay, percent, quotaCounter, quotaPrice) {
-    alert("Monto a pagar de "+totalPay+"$ en "+quotaCounter+" cuotas, con un interes del "+percent+"% sobre el total. \n\n Valor de cada cuota "+quotaPrice+"$");
-}
-function creditWithPercent(quotaCounter, totalPay) {
-    let result = 0;
-    if (quotaCounter <= 3) {
-        percent = 5;
-        result = totalPay * 0.05;
-        result = totalPay + result;
-        return result;
-    } else if (quotaCounter > 3 && quotaCounter <= 8) {
-        percent = 10;
-        result = totalPay * 0.1;
-        result = totalPay + result;
-        return result;
+let total = 0;
+let carrito = [];
+let nombreCliente;
+let fechaCompra;
+let metodoDePago;
+
+function agregarProducto(producto, cantidad) {
+    if (productos[producto]) {
+        total += productos[producto] * cantidad;
+        carrito.push({ producto, cantidad });
     } else {
-        percent = 15;
-        result = totalPay * 0.15;
-        result = totalPay + result;
-        return result;
+        alert("Producto no encontrado");
     }
 }
-function systemExit(confirm) {
-    if (confirm !== "si") {
-        active = false;
-        alert(" Hasta la proxima compra ")
-    }
+
+function mostrarResumen() {
+    alert("Resumen de la compra: " + carrito.map(item => `${item.producto} x ${item.cantidad}`).join(" + "));
 }
-/* main */
-let percent = 0;
-let active = true;
-do {
-    let totalPay = parseInt(prompt("Ingrese el monto a cobrar"));
-    if (parseInt(totalPay)) {
-        let paymentType = prompt("Como le gustaria abonar: credito o al contado?").toLowerCase();
-        switch(paymentType) {
-            case "contado":
-                msgTotalPayCounted(totalPay);
-                break;
-            case "credito":
-                let quotaCounter = parseInt(prompt("En cuantas cuotas desea abonar?"));
-                if (parseInt(quotaCounter)) {
-                    totalPay = creditWithPercent(quotaCounter, totalPay);
-                    console.log(totalPay)
-                    quotaPrice = parseFloat(totalPay / quotaCounter).toFixed(3);
-                    msgTotalPayCredit(totalPay, percent, quotaCounter, quotaPrice);
-                } else {
-                    alert("El valor ingresado es incorrcto para la cuota");
-                }
-                break;
-            default:
-                console.error("Usted selecciono")
-                break;
-        }
-    } else {
-        alert("No ingreso un monto correcto");
+
+function calcularRecargo(total) {
+    return total * 0.10;
+}
+
+function calcularImpuesto(total) {
+    return total * 0.16;
+}
+
+function calcularDescuento(total) {
+    return total * 0.05;
+}
+
+function calcularTotalFinal(total, recargo, impuesto, descuento) {
+    return total + recargo - descuento + impuesto;
+}
+
+nombreCliente = prompt("Indique su nombre");
+fechaCompra = prompt("Ingrese fecha de su compra");
+
+while (true) {
+    const producto = prompt("Seleccione un producto: " + Object.keys(productos).join(", ") + " o C para cerrar");
+    if (producto === "C") 
+        break;
+    const cantidad = parseInt(prompt("Ingrese la cantidad de su producto"));
+    if (isNaN(cantidad) || cantidad <= 0) {
+        alert("La es Cantidad inválida");
+        continue;
     }
-    let confirm = prompt("Desea realizar otro pago? si o no").toLowerCase();
-    systemExit(confirm);
-} while (active);
+    agregarProducto(producto, cantidad);
+}
+
+mostrarResumen();
+
+metodoDePago = prompt("Seleccione medio de pago:  1 Efectivo, 2 Tarjeta");
+if (metodoDePago === "1") {
+    const recargo = 0;
+    const impuesto = calcularImpuesto(total);
+    const descuento = calcularDescuento(total);
+    const totalFinal = calcularTotalFinal(total, recargo, impuesto, descuento);
+    alert("Compra finalizada: Nombre del cliente: " + nombreCliente + " Fecha de la compra: " + fechaCompra + " Total: $" + totalFinal);
+} else if (metodoDePago === "2") {
+    const recargo = calcularRecargo(total);
+    const impuesto = calcularImpuesto(total);
+    const descuento = calcularDescuento(total);
+    const totalFinal = calcularTotalFinal(total, recargo, impuesto, descuento);
+    alert("La Compra se realizo con exito: Nombre del cliente: " + nombreCliente + " Fecha de la compra: " + fechaCompra + " Total: $" + totalFinal);
+} else {
+    alert("Medio de pago no válido");
+}
